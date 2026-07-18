cb217af696c7966715d5a13967592fc83265db65
miniorange <kulkarninabha4@gmail.com>
2026-07-18 12:20:14 +0530

    chore(product-service): configure TypeScript build settings

---
+++ services/product-service/src/repositories/product.repository.ts
@@ -0,0 +1,43 @@
+import { prisma } from "../config/prisma";
+
+export async function createProduct(data: any) {
+    return prisma.product.create({
+        data,
+    });
+}
+
+
+export async function getProducts() {
+    return prisma.product.findMany();
+}
+
+
+export async function getProductById(id: string) {
+    return prisma.product.findUnique({
+        where: {
+            id,
+        },
+    });
+}
+
+
+export async function updateProduct(
+    id: string,
+    data: any
+) {
+    return prisma.product.update({
+        where: {
+            id,
+        },
+        data,
+    });
+}
+
+
+export async function deleteProduct(id: string) {
+    return prisma.product.delete({
+        where: {
+            id,
+        },
+    });
+}
